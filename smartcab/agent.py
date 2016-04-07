a179e86ac366b2f9b23158af2c11b5f0404970ca
HenryDev <HenryDev@users.noreply.github.com>
2016-04-07 14:10:38 -0500

    test travis build

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -7,20 +7,26 @@ import itertools
 
 
 class LearningAgent(Agent):
-    """An agent that learns to drive in the smartcab world."""
-
     def __init__(self, env):
-        # sets self.env = env, state = None, next_waypoint = None, and a default color
         super(LearningAgent, self).__init__(env)
-        self.color = 'red'  # override color
-        self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
+        self.color = 'red'
+        self.planner = RoutePlanner(self.env, self)
         valid_actions = Environment.valid_actions
         self.q_values = pandas.DataFrame(columns=valid_actions,
-                                         index=itertools.product(['red', 'green'], valid_actions)).fillna(3.14159265)
+                                         index=itertools.product(['red', 'green'],
+                                                                 valid_actions,  # 4 way intersection
+                                                                 valid_actions,
+                                                                 valid_actions,
+                                                                 valid_actions)).fillna(3141.59265)
         self.deadline_tracker = []
         self.trip_reward = 0
         self.trip_number = 0
         self.rewards = []
+
+    def get_current_state(self):
+        inputs = self.env.sense(self)
+        state = (inputs['light'], self.next_waypoint, inputs['oncoming'], inputs['left'], inputs['right'])
+        return state
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
@@ -29,21 +35,15 @@ class LearningAgent(Agent):
         self.trip_number += 1
 
     def update(self, t):
-        # Gather inputs
-        self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
-        inputs = self.env.sense(self)
+        self.next_waypoint = self.planner.next_waypoint()
         deadline = self.env.get_deadline(self)
         self.deadline_tracker.append(self.trip_number) if deadline < 1 else 0
-        self.state = (inputs['light'], self.next_waypoint)
-
+        self.state = self.get_current_state()
         action, q_value = self.explore_exploit(self.state)
-
-        # Execute action and get reward
         reward = self.env.act(self, action)
         self.trip_reward += reward
         self.update_q_value(reward, q_value, self.state, action)
-        print "deadline = {}, inputs = {}, action = {}, reward = {}, trip# = {}" \
-            .format(deadline, inputs, action, reward, self.trip_number)
+        print "deadline = {}, action = {}, reward = {}, trip# = {}".format(deadline, action, reward, self.trip_number)
 
     def choose_action_and_q_value(self, state):
         row = self.q_values.loc[[state]]
@@ -54,15 +54,10 @@ class LearningAgent(Agent):
     def update_q_value(self, reward, q_value, state, action):
         discount_factor = 0.5
         learning_rate = 0.8
-
-        new_state = (self.env.sense(self)['light'], self.next_waypoint)
+        new_state = self.get_current_state()
         new_q = self.choose_action_and_q_value(new_state)[1]
-
-        new_q = q_value + learning_rate * (reward + discount_factor * new_q - q_value)
-        self.q_values.set_value(new_state, action, new_q)
-
-        q_value += learning_rate * (reward + discount_factor * new_q - q_value)
-        self.q_values.set_value(state, action, q_value)
+        new_q = (1 - learning_rate) * q_value + learning_rate * (reward + discount_factor * new_q)
+        self.q_values.set_value(state, action, new_q)
 
     def explore_exploit(self, state):
         explore_chance = 1 / (self.trip_number + 0.9)
@@ -75,16 +70,11 @@ class LearningAgent(Agent):
 
 
 def run():
-    """Run the agent for a finite number of trials."""
-
-    # Set up environment and agent
-    e = Environment()  # create environment (also adds some dummy traffic)
-    a = e.create_agent(LearningAgent)  # create agent
+    e = Environment()
+    a = e.create_agent(LearningAgent)
     e.set_primary_agent(a, enforce_deadline=True)  # set agent to track
-
-    # Now simulate it
     sim = Simulator(e, update_delay=0.01)  # reduce update_delay to speed up simulation
-    sim.run(n_trials=100)  # press Esc or close pygame window to quit
+    sim.run(n_trials=250)  # press Esc or close pygame window to quit
     print 'trip rewards: {}'.format(a.rewards)
     print 'Missed deadlines: {}'.format(a.deadline_tracker)
 
--- smartcab/test_agent.py
+++ smartcab/test_agent.py
@@ -4,62 +4,58 @@ from environment import Environment
 
 
 class TestAgent(unittest.TestCase):
+    green_forward_state = ('green', 'forward', None, None, None)
+    red_right_state = ('red', 'right', None, None, None)
+    red_forward_state = ('red', 'forward', None, None, None)
+
     def setUp(self):
         env = Environment()
         self.agent = env.create_agent(agent.LearningAgent)
+        self.agent.q_values[self.agent.q_values > 0] = 0.1
 
     def testChooseActionShouldReturnActionWithHighestQValue(self):
-        state = ('green', 'forward')
-        self.agent.q_values.set_value(state, 'left', 10)
-        self.assertEqual(self.agent.choose_action_and_q_value(state)[0], 'left')
+        self.agent.q_values.set_value(self.green_forward_state, 'left', 10)
+        self.assertEqual(self.agent.choose_action_and_q_value(self.green_forward_state)[0], 'left')
 
     def testChooseActionShouldWorkWithMultipleSameValues(self):
-        state = ('green', 'forward')
-        self.agent.q_values.set_value(state, 'left', 10)
-        self.agent.q_values.set_value(state, 'right', 10)
-        self.assertEqual(self.agent.choose_action_and_q_value(state)[0], 'left')
+        self.agent.q_values.set_value(self.green_forward_state, 'left', 10)
+        self.agent.q_values.set_value(self.green_forward_state, 'right', 10)
+        self.assertEqual(self.agent.choose_action_and_q_value(self.green_forward_state)[0], 'left')
 
     def testChooseActionShouldNotCareAboutOtherStates(self):
-        state = ('green', 'forward')
-        state1 = ('red', 'forward')
-        self.agent.q_values.set_value(state, 'left', 10)
-        self.agent.q_values.set_value(state1, 'right', 100)
-        self.assertEqual(self.agent.choose_action_and_q_value(state)[0], 'left')
-        self.assertNotEqual(self.agent.choose_action_and_q_value(state)[0], 'right')
+        self.agent.q_values.set_value(self.green_forward_state, 'left', 10)
+        self.agent.q_values.set_value(self.red_forward_state, 'right', 100)
+        self.assertEqual(self.agent.choose_action_and_q_value(self.green_forward_state)[0], 'left')
+        self.assertNotEqual(self.agent.choose_action_and_q_value(self.green_forward_state)[0], 'right')
 
     def testChooseActionShouldWorkWithNegativeValues(self):
-        state = ('green', 'forward')
-        self.agent.q_values.set_value(state, 'left', 10.01)
-        self.agent.q_values.set_value(state, 'right', -10)
-        self.assertEqual(self.agent.choose_action_and_q_value(state)[0], 'left')
+        self.agent.q_values.set_value(self.green_forward_state, 'left', 10.01)
+        self.agent.q_values.set_value(self.green_forward_state, 'right', -10)
+        self.assertEqual(self.agent.choose_action_and_q_value(self.green_forward_state)[0], 'left')
 
     def testUpdateShouldUpdateQValues(self):
-        state = ('green', 'forward')
         old_q_value = 5.5
         action = 'left'
-        self.agent.q_values.set_value(state, action, old_q_value)
-        self.agent.update_q_value(1, old_q_value, state, action)
-        new_q_value = self.agent.q_values.loc[[state]][action][0]
+        self.agent.q_values.set_value(self.green_forward_state, action, old_q_value)
+        self.agent.update_q_value(1, old_q_value, self.green_forward_state, action)
+        new_q_value = self.agent.q_values.loc[[self.red_right_state]][action][0]
         self.assertNotEqual(new_q_value, old_q_value)
 
     def testExploreExploitShouldWorkOnFirstTrip(self):
-        state = ('green', 'forward')
         self.agent.trip_number = 0
-        self.assertTrue(self.agent.explore_exploit(state)[0] in Environment.valid_actions)
-        self.assertTrue(self.agent.explore_exploit(state)[1] > 0)
+        self.assertTrue(self.agent.explore_exploit(self.green_forward_state)[0] in Environment.valid_actions)
+        self.assertTrue(self.agent.explore_exploit(self.green_forward_state)[1] > 0)
 
     def testChooseActionShouldReturnCorrectQValue(self):
-        state = ('green', 'forward')
-        self.agent.q_values.set_value(state, 'left', 10.123)
-        self.assertEqual(self.agent.choose_action_and_q_value(state)[0], 'left')
-        self.assertEqual(self.agent.choose_action_and_q_value(state)[1], 10.123)
+        self.agent.q_values.set_value(self.green_forward_state, 'left', 10.123)
+        self.assertEqual(self.agent.choose_action_and_q_value(self.green_forward_state)[0], 'left')
+        self.assertEqual(self.agent.choose_action_and_q_value(self.green_forward_state)[1], 10.123)
 
     def testExploreExploitShouldWorkOnBillionthTrip(self):
-        state = ('green', 'forward')
         self.agent.trip_number = 1000000000
-        self.agent.q_values.set_value(state, 'left', 10.123)
-        self.assertEqual(self.agent.choose_action_and_q_value(state)[0], 'left')
-        self.assertEqual(self.agent.choose_action_and_q_value(state)[1], 10.123)
+        self.agent.q_values.set_value(self.green_forward_state, 'left', 10.123)
+        self.assertEqual(self.agent.choose_action_and_q_value(self.green_forward_state)[0], 'left')
+        self.assertEqual(self.agent.choose_action_and_q_value(self.green_forward_state)[1], 10.123)
 
 
 if __name__ == '__main__':
